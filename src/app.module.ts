da65d7e0471f197667d6b5f7781b50f924a5692e
jin.xiao <jin.xiao@shopee.com>
2022-02-27 23:43:05 +0800

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { AudioModule } from './jobs/audio/audio.module';
+import { AlbumModule } from './modules/album/album.module';
 
 @Module({
   imports: [
@@ -37,6 +38,7 @@ import { AudioModule } from './jobs/audio/audio.module';
     UsersModule,
     TasksModule,
     AudioModule,
+    AlbumModule,
   ],
 })
 export class AppModule {
---
+++ src/modules/album/album.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  Get,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AlbumService } from './album.service';
+
+@Controller()
+export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file) {
+    this.albumService.upload(file);
+    return true;
+  }
+
+  @Get('export')
+  async download(@Res() res: Response) {
+    const { filename, tarStream } = await this.albumService.downloadAll();
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+    tarStream.pipe(res);
+  }
+}
